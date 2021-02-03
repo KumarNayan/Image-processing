a94cee241bd43e9967473d35e2547d7bae0751d2
Kumar Nayan <nayanr489@gmail.com>
2021-02-03 22:05:25 +0530

    changed file name

--- image_processing.py
+++ image_processing.py
@@ -1,4 +1,4 @@
-# Convolutional Neural Network
+# Image Processing using Convolutional Neural Network
 
 # Importing the libraries
 import tensorflow as tf
@@ -61,13 +61,13 @@ cnn.fit(x= training_set, validation_data= test_set, epochs=25)
 # Predicting a single image
 import numpy as np
 from keras.preprocessing import image
-test_image = image.load_img('dataset/single_prediction/pp1.jpg', target_size = (64, 64))
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = cnn.predict(test_image)
 training_set.class_indices
 if result[0][0] == 1:
-    prediction = 'bitch'
+    prediction = "It's a doggo!"
 else:
-    prediction = 'cat'
+    prediction = "It's a pussy cat!"
 print(prediction)
